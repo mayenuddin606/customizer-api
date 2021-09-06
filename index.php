928e6d5b0999fd62a4ed832449dde91fbfadfbd6
mayenuddin606 <85036260+mayenuddin606@users.noreply.github.com>
2021-09-06 20:24:05 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<img src="<?php echo get_theme_mod('logo-uploader'); ?>" alt="image">
+
+<?php if(true === get_theme_mod('copyright-show')) : ?>
+
+<h2 class="copyright-text">Copyright: <?php echo get_theme_mod('copyright-text'); ?></h2>
+
+<?php endif; ?>
+<h2 class="heroine">Select Heroine: <?php echo get_theme_mod('select-heroine'); ?></h2>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
